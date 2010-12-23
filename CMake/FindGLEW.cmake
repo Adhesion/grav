8c430f0ea6324224cf775126e87eb0ed85ff6618
Andrew Ford <acfpop@rit.edu>
2010-12-23 12:01:56 -0500

    modified glew script to use pkg-config

--- CMake/FindGLEW.cmake
+++ CMake/FindGLEW.cmake
@@ -5,24 +5,38 @@
 # Created on: Dec 22, 2010
 # @author Andrew Ford
 
-find_path(
-	GLEW_INCLUDE_DIRS
-	GL/ftgl.h
-	HINTS ${GLEW_HOME}/include
-	)
+include(FindPkgConfig)
 
-find_library(
-	GLEW_LIBRARIES
-	glew
-	HINTS ${GLEW_HOME}/lib
-	)
+if(PKG_CONFIG_FOUND)
+	message("Doing pkg config glew check...")
+	pkg_check_modules(GLEW
+		glew>=1.5
+		)
+endif()
 
-include(FindPackageHandleStandardArgs)
+if(NOT GLEW_FOUND)
+	message("Warning: could not confirm GLEW version (pkg-config not available).")
+	message("Ensure your GLEW version is >= 1.5 for a clean build.")
 
-find_package_handle_standard_args(
-	GLEW
-	DEFAULT_MSG
-	GLEW_INCLUDE_DIRS
-	GLEW_LIBRARIES
-	)
+	find_path(
+		GLEW_INCLUDE_DIRS
+		GL/glew.h
+		HINTS ${GLEW_HOME}/include
+		)
+
+	find_library(
+		GLEW_LIBRARIES
+		GLEW
+		HINTS ${GLEW_HOME}/lib
+		)
+
+	include(FindPackageHandleStandardArgs)
+
+	find_package_handle_standard_args(
+		GLEW
+		DEFAULT_MSG
+		GLEW_INCLUDE_DIRS
+		GLEW_LIBRARIES
+		)
+endif()
 
