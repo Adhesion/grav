a1124875d7c338a129ea882737a28752b485c377
Andrew Ford <acfpop@rit.edu>
2011-03-23 16:14:10 -0400

    gitignore old eclipse build dir

--- Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lGL -lGLU -lGLEW -lftgl -lpng -lVPMedia -luclmmbase -lavcodec -lavutil -lswscale -lpython2.6
