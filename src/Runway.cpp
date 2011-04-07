e1217875c0c7c89c5532a518c9d488999df90cf4
Andrew Ford <acfpop@rit.edu>
2011-04-07 12:50:48 -0400

    style fixes in runway

--- src/Runway.cpp
+++ src/Runway.cpp
@@ -92,7 +92,7 @@ void Runway::rearrange()
 {
     if ( objects.size() == 0 ) return;
 
-    std::map<std::string, std::string> opts = \
+    std::map<std::string, std::string> opts =
         std::map<std::string, std::string>();
     // horizontal
     if ( orientation == 0 )
@@ -102,14 +102,14 @@ void Runway::rearrange()
         opts["numX"] = ss.str();
         opts["numY"] = "1";
 
-        std::map<std::string, std::vector<RectangleBase*> > data = \
+        std::map<std::string, std::vector<RectangleBase*> > data =
             std::map<std::string, std::vector<RectangleBase*> >();
         data["objects"] = objects;
 
-        layouts.arrange("grid",
-                        getLBound(), getRBound(), getUBound(), getDBound(),
-                        0, 0, 0, 0,
-                        data, opts);
+        layouts.arrange( "grid",
+                getLBound(), getRBound(), getUBound(), getDBound(),
+                0, 0, 0, 0,
+                data, opts );
     }
     // vertical
     else if ( orientation == 1 )
@@ -124,10 +124,10 @@ void Runway::rearrange()
             std::map<std::string, std::vector<RectangleBase*> >();
         data["objects"] = objects;
 
-        layouts.arrange("grid",
-                        getLBound(), getRBound(), getUBound(), getDBound(),
-                        0, 0, 0, 0,
-                        data, opts);
+        layouts.arrange( "grid",
+                getLBound(), getRBound(), getUBound(), getDBound(),
+                0, 0, 0, 0,
+                data, opts );
     }
 }
 
