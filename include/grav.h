508a42943e49c357e92ed8e59439ec0f1b1ce7de
Andrew Ford <acfpop@rit.edu>
2011-04-08 15:17:53 -0400

    start adding license/copyright notices

--- include/grav.h
+++ include/grav.h
@@ -1,6 +1,3 @@
-#ifndef GRAV_H_
-#define GRAV_H_
-
 /**
  * @file grav.h
  *
@@ -9,7 +6,26 @@
  * main().
  *
  * @author Andrew Ford
+ * Copyright (C) 2011 Rochester Institute of Technology
+ *
+ * This file is part of grav.
+ *
+ * grav is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * grav is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with grav.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#ifndef GRAV_H_
+#define GRAV_H_
 
 #include <wx/wx.h>
 #include <wx/cmdline.h>
